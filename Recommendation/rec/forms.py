3872ae96fb5d0887991fd18ce911667a8be4590a
Nikos Tzagkarakis <tzagarakis3@gmail.com>
2021-01-04 22:13:47 +0200

    SearchBox added

--- Recommendation/rec/forms.py
+++ Recommendation/rec/forms.py
@@ -1,13 +1,12 @@
 from django import forms
-from django_select2 import forms as s2forms
 
 
 class CityForm(forms.Form):
     def __init__(self, *args, **kwargs):
         cities = kwargs.pop("City")
         super(CityForm, self).__init__(*args, **kwargs)
-        self.fields["City"] = forms.ChoiceField(choices=cities, label='City name        ',
-                                                widget=s2forms.Select2Widget(attrs={'onchange': 'submit();'}))
+        self.fields["City"] = forms.ChoiceField(choices=cities, label='City name',
+                                                widget=forms.Select(attrs={'onchange': 'submit();'}))
 
 
 class CategoryForm(forms.Form):
@@ -16,5 +15,3 @@ class CategoryForm(forms.Form):
         super(CategoryForm, self).__init__(*args, **kwargs)
         self.fields["Category"] = forms.MultipleChoiceField(choices=categories, label='Categories')
         # widget=forms.CheckboxSelectMultiple())
-
-
