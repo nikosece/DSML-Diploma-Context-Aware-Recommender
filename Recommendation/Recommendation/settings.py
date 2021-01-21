7d883c1fafecc1d9523aef53608348664cc60d8d
Nikos Tzagkarakis <tzagarakis3@gmail.com>
2021-01-21 14:05:26 +0200

    Add postgre Database

--- Recommendation/Recommendation/settings.py
+++ Recommendation/Recommendation/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'Recommendation.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'diploma',
+        'USER': 'nikos',
+        'PASSWORD': 'ntua',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
+
 }
 
 
--- Recommendation/rec/models.py
+++ Recommendation/rec/models.py
@@ -1,17 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.postgres.fields import ArrayField
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-
+import pickle
+from django import forms
 from .managers import CustomUserManager
+
+
+class ChoiceArrayField(ArrayField):
+    """
+    A field that allows us to store an array of choices.
+    Uses Django's Postgres ArrayField
+    and a MultipleChoiceField for its formfield.
+    """
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': forms.MultipleChoiceField,
+            'choices': self.base_field.choices,
+        }
+        defaults.update(kwargs)
+        # Skip our parent's formfield implementation completely as we don't
+        # care for it.
+        # pylint:disable=bad-super-call
+        return super(ArrayField, self).formfield(**defaults)
+
+
+category_list = pickle.load(open("/home/anonymous/Documents/Diploma-Recommender/Recommendation/Dataset"
+                                 "/category_list.pickle", "rb"))
+category_tuple = ((),)
+for j in category_list:
+    category_tuple = category_tuple + ((j, j),)
+category_tuple = list(category_tuple)
+category_tuple.pop(0)
+category_tuple = tuple(category_tuple)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    preference = models.CharField(max_length=400)
+    preference = ChoiceArrayField(
+        base_field=models.CharField(max_length=256, choices=category_tuple),
+        default=list)
+
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
