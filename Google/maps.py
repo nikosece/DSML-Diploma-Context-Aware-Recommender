cc88b323d3c8e069c5deaa621ede28ee7a46d32c
Nikos Tzagkarakis <tzagarakis3@gmail.com>
2021-01-29 22:02:23 +0200

    4376 businesses in 35 areas

--- Google/maps.py
+++ Google/maps.py
@@ -9,14 +9,13 @@ def read_pickle(name):
     return pickle.load(open(name, "rb"))
 
 
-def plot(df):
+def plot2(df, my_map, feature_name):
     fields = ["Name", "Stars", "Review Count"]
     lat_list = df[0]
     long_list = df[1]
     names_list = df[2]
     stars_list = df[3]
     count_list = df[4]
-    my_map = folium.Map(location=[lat_list[0], long_list[0]], zoom_start=12, prefer_canvas=True)
     my_list = list()
     for lati, lng, name, star, count in zip(lat_list, long_list, names_list, stars_list, count_list):
         my_dict = dict()
@@ -27,51 +26,59 @@ def plot(df):
     geojson_file = dict()
     geojson_file["type"] = "FeatureCollection"
     geojson_file["features"] = my_list
-    geo = folium.GeoJson(geojson_file, name="Location",
-                         tooltip=folium.GeoJsonTooltip(fields=fields))
+    geo = folium.GeoJson(geojson_file, name=feature_name,
+                         tooltip=folium.GeoJsonTooltip(fields=fields), show=False)
     my_map.add_child(geo)
-    Search(
-        layer=geo,
-        geom_type='Point',
-        placeholder='Search for a place',
-        search_label="Name",
-        search_zoom=18,
-        collapsed=False,
-    ).add_to(my_map)
+    return my_map
+
+
+def plot():
     mark = folium.FeatureGroup("Borders")
     with open('track_requests') as f:
         for line in f:
             *arg, area = line.rstrip('\n').split(" ")
             mark.add_child(folium.Circle((arg[0], arg[1]), radius=arg[2]))
+    my_map = folium.Map(location=[arg[0], arg[1]], zoom_start=12, prefer_canvas=True)
     my_map.add_child(mark)
-    folium.LayerControl().add_to(my_map)
-    my_map.save("map.html")
+    return my_map
 
 
-res = []
+all_res = []
+area_names = []
 for file in os.listdir("./"):
     if file.endswith(".pickle"):
-        if len(res) == 0:
-            res = read_pickle(file)
-        else:
-            res.extend(read_pickle(file))
+        all_res.append(read_pickle(file))
+        area_names.append(file.strip(".pickle"))
+init_map = plot()
+all_res = [x for _, x in sorted(zip(area_names, all_res))]
+area_names.sort()
+for res, display in zip(all_res, area_names):
+    lat = []
+    lot = []
+    names = []
+    stars = []
+    r_count = []
+    for r in res:
+        lat.append(r['geometry']['location']['lat'])
+        lot.append(r['geometry']['location']['lng'])
+        names.append(r['name'])
+        try:
+            stars.append(r['rating'])
+        except KeyError:
+            stars.append("None")
+        try:
+            r_count.append(r['user_ratings_total'])
+        except KeyError:
+            r_count.append("None")
+    init_map = plot2([lat, lot, names, stars, r_count], init_map, display)
+# Search(
+#     layer=geo,
+#     geom_type='Point',
+#     placeholder='Search for a place',
+#     search_label="Name",
+#     search_zoom=18,
+#     collapsed=False,
+# ).add_to(init_map)
 
-lat = []
-lot = []
-names = []
-stars = []
-r_count = []
-print("Total results: ", len(res))
-for r in res:
-    lat.append(r['geometry']['location']['lat'])
-    lot.append(r['geometry']['location']['lng'])
-    names.append(r['name'])
-    try:
-        stars.append(r['rating'])
-    except KeyError:
-        stars.append("None")
-    try:
-        r_count.append(r['user_ratings_total'])
-    except KeyError:
-        r_count.append("None")
-plot([lat, lot, names, stars, r_count])
+folium.LayerControl().add_to(init_map)
+init_map.save("map.html")
