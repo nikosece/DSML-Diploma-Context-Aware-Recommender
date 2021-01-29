deeeac7409827548cb5ba69e5bc1c2f10b999963
Nikos Tzagkarakis <tzagarakis3@gmail.com>
2021-01-29 16:56:51 +0200

    Google places

---
+++ Google/maps.py
@@ -0,0 +1,77 @@
+import json
+import folium
+from folium.plugins import Search
+import os
+import pickle
+
+
+def read_pickle(name):
+    return pickle.load(open(name, "rb"))
+
+
+def plot(df):
+    fields = ["Name", "Stars", "Review Count"]
+    lat_list = df[0]
+    long_list = df[1]
+    names_list = df[2]
+    stars_list = df[3]
+    count_list = df[4]
+    my_map = folium.Map(location=[lat_list[0], long_list[0]], zoom_start=17, prefer_canvas=True)
+    my_list = list()
+    for lati, lng, name, star, count in zip(lat_list, long_list, names_list, stars_list, count_list):
+        my_dict = dict()
+        my_dict["type"] = "Feature"
+        my_dict["geometry"] = {"type": "Point", "coordinates": [lng, lati]}
+        my_dict["properties"] = {"Name": name, "Stars": star, "Review Count": count}
+        my_list.append(my_dict)
+    geojson_file = dict()
+    geojson_file["type"] = "FeatureCollection"
+    geojson_file["features"] = my_list
+    geo = folium.GeoJson(geojson_file, name="Location",
+                         tooltip=folium.GeoJsonTooltip(fields=fields))
+    my_map.add_child(geo)
+    Search(
+        layer=geo,
+        geom_type='Point',
+        placeholder='Search for a place',
+        search_label="Name",
+        search_zoom=18,
+        collapsed=False,
+    ).add_to(my_map)
+    mark = folium.FeatureGroup("Borders")
+    with open('track_requests') as f:
+        for line in f:
+            *arg, area = line.rstrip('\n').split(" ")
+            mark.add_child(folium.Circle((arg[0], arg[1]), radius=arg[2]))
+    my_map.add_child(mark)
+    folium.LayerControl().add_to(my_map)
+    my_map.save("map.html")
+
+
+res = []
+for file in os.listdir("./"):
+    if file.endswith(".pickle"):
+        if len(res) == 0:
+            res = read_pickle(file)
+        else:
+            res.extend(read_pickle(file))
+
+lat = []
+lot = []
+names = []
+stars = []
+r_count = []
+print("Total results: ", len(res))
+for r in res:
+    lat.append(r['geometry']['location']['lat'])
+    lot.append(r['geometry']['location']['lng'])
+    names.append(r['name'])
+    try:
+        stars.append(r['rating'])
+    except KeyError:
+        stars.append("None")
+    try:
+        r_count.append(r['user_ratings_total'])
+    except KeyError:
+        r_count.append("None")
+plot([lat, lot, names, stars, r_count])
---
+++ Google/places_api.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import pickle
+import time
+
+
+def save_pickle(var, name):
+    with open(name + '.pickle', 'wb') as fle:
+        pickle.dump(var, fle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def deletion(a, b):
+    del_list = set()
+    for i in a:
+        for j in range(len(b)):
+            if i['place_id'] == b[j]['place_id']:
+                del_list.add(j)
+    b = [i for j, i in enumerate(b) if j not in del_list]
+    a.extend(b)
+    return a
+
+
+def get_data(latitude, longitude, radius, search, pagetoken=None):
+    api_key = 'AIzaSyDZVMPpjO-_nBgUkWc-9VWUnFxyo0LBbqI'
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    url = url + 'location=' + latitude + ',' + longitude + '&radius=' + radius + '&type=' + search + '&key=' + api_key
+    if pagetoken:
+        url = url + '&pagetoken=' + pagetoken
+    time.sleep(2.5)
+    r = requests.get(url)
+    x = r.json()
+    y = x['results']
+    if 'next_page_token' in x:
+        y_new = get_data(latitude, longitude, radius, search, x['next_page_token'])
+        y.extend(y_new)
+        return y
+    else:
+        return y
+
+
+with open('requests') as f:
+    for line in f:
+        *arg, area = line.rstrip('\n').split(" ")
+        search_type = 'cafe'
+        y_c = get_data(*arg, search_type)
+        search_type = 'restaurant'
+        y_r = get_data(*arg, search_type)
+        y_c = deletion(y_c, y_r)
+        search_type = 'bar'
+        y_b = get_data(*arg, search_type)
+        y_c = deletion(y_c, y_b)
+        save_pickle(y_c, area)
