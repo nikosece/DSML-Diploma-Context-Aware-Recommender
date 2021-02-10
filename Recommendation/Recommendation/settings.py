4a48c932da3700638a87814f9f06d610c0e699b5
Nikos Tzagkarakis <tzagarakis3@gmail.com>
2021-02-10 20:18:36 +0200

    photos add, mobile friendly

--- Recommendation/Recommendation/settings.py
+++ Recommendation/Recommendation/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,  'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT = '/home/anonymous/Documents/media'
+MEDIA_URL = '/media/'
